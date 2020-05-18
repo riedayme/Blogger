22b24ba0877214c41ee3fa7ed95caf7e89b6390e
riedayme <programmer.irfaan@gmail.com>
2020-05-19 06:17:54 +0700

    update

--- RidayFeed/assets/js/custom.js
+++ RidayFeed/assets/js/custom.js
@@ -9,6 +9,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
     for (var i = 0; i < pres.length; i++) {
         hljs.highlightBlock(pres[i]);
     }
+
+    /* double click */
+    for (var pres = document.querySelectorAll("pre,code,kbd,blockquote,td"), i = 0; i < pres.length; i++) 
+        pres[i].addEventListener("dblclick", function() {
+            var e = getSelection(),
+            t = document.createRange();
+            t.selectNodeContents(this), e.removeAllRanges(), e.addRange(t)
+        }, !1);
+
     var options = {
         contentSelector: ".blog-posts",
         loadDelay:0,
