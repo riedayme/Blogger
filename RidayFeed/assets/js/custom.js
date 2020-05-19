58b51bd7896afc370e027fa3f87104cb66777890
riedayme <programmer.irfaan@gmail.com>
2020-05-19 10:07:13 +0700

    update

--- RidayFeed/assets/js/custom.js
+++ RidayFeed/assets/js/custom.js
@@ -184,8 +184,8 @@ function kodein_MoveByID(e, n) {
 /**
 * Adsense LazyLoad 
 */
-if (adsense_status == 'true') {
-    if(adsense_lazyload_status == 'true') {                            
+if (adsense_status == 1) {
+    if(adsense_lazyload_status == 1) {                            
         var lazyAdsense = false;
         window.addEventListener("scroll", function(){
             if ((document.documentElement.scrollTop != 0 && lazyAdsense === false) || (document.body.scrollTop != 0 && lazyAdsense === false)) {
@@ -220,4 +220,114 @@ window.cookieconsent.initialise({
         "dismiss": cookie_dismiss,
         "link": cookie_link
     }
+}); 
+
+/**
+ * Related Post
+ */
+ var allrelatedfeedorigin = new Array;
+
+ function removetag(e, t) {
+    for (var i = e.split("<"), n = 0; n < i.length; n++) - 1 != i[n].indexOf(">") && (i[n] = i[n].substring(i[n].indexOf(">") + 1, i[n].length));
+        return i = (i = i.join("")).substring(0, t - 1)
+}
+
+function extractfeed(e) {
+    var t = e.feed.entry,
+    n = t.length,
+    r = new Array;
+    for (r[1] = "Jan", r[2] = "Feb", r[3] = "Mar", r[4] = "Apr", r[5] = "May", r[6] = "Jun", r[7] = "Jul", r[8] = "Aug", r[9] = "Sep", r[10] = "Oct", r[11] = "Nov", r[12] = "Dec", i = 0; i < n; i++) {
+        var a, l = t[i].title.$t,
+        s = t[i].published.$t,
+        u = s.substring(0, 4),
+        o = s.substring(5, 7),
+        f = s.substring(8, 10),
+        g = (s.substring(11, 16), f + " " + r[parseInt(o, 10)] + "," + u);
+        "summary" in t[i] ? a = t[i].summary.$t : "content" in t[i] && (a = t[i].content.$t);
+        var m = removetag(a, 150);
+        if ("media$thumbnail" in t[i]) h = (h = t[i].media$thumbnail.url).replace("/s72-c/", "/s1600/");
+        else var h = "https://1.bp.blogspot.com/-P6sIgHwSrZU/XrncUn1POBI/AAAAAAAABK4/cpGgDNNzCuIDFXn5Uqmey4qh23efe-f_QCLcBGAsYHQ/s1600/no-image-compress.jpg";
+        for (var c = h, v = 0; v < t[i].link.length; v++){
+            
+            if ("alternate" == t[i].link[v].rel) {
+                var A = t[i].link[v].href;
+                break
+            } 
+            var b = t[i].author[0].name.$t;
+            allrelatedfeedorigin.push({
+                title: l,
+                link: A,
+                images: c,
+                author: b,
+                date: g,
+                snippet: m
+            }), 0
+        }
+    }
+}
+
+function shuffle(e) {
+    for (var t, i, n = e.length; 0 !== n;) i = Math.floor(Math.random() * n), t = e[n -= 1], e[n] = e[i], e[i] = t;
+        return e
+}
+const removeDupliactes = e => {
+    let t = e.map(e => Object.values(e).join(""));
+    return e.filter((e, i) => t.indexOf(t[i]) === i)
+};
+
+function createrelated(){
+    var maxrelated = 6;
+    write = document.getElementById('related-post-write');
+    var allrelatedfeed = shuffle(removeDupliactes(allrelatedfeedorigin));
+    var allrelatedfeedlength = allrelatedfeed.length;
+    var url_path = location.protocol + '//' + location.host + location.pathname;
+    for (var xx = 0; xx < allrelatedfeedlength; xx++) {
+        if (allrelatedfeedlength != 1 && allrelatedfeed[xx].link != url_path) {
+            $("#related-post-title-write").removeClass('u-hidden-visually');
+        }
+        if (allrelatedfeed[xx].link !== url_path) {
+            html = '<div class="col-lg-4 col-md-6 col-sm-6 col-6 u-ph-xsmall u-mb-xsmall">';
+            html += '<article class="c-event u-m-zero u-p-zero">';
+            html += '<div class="c-event__img u-m-zero">';
+            html += '   <a href="'+allrelatedfeed[xx].link+'" title="'+allrelatedfeed[xx].title+'" style="display:grid">';
+            html += '       <img class="c-post-image" src="'+allrelatedfeed[xx].images+'" alt="'+allrelatedfeed[xx].title+'"/>';
+            html += '   </a>';
+            html += '</div>';
+            html += '<div class="c-event__meta u-ph-small u-pt-xsmall related-cards-title" data-mh="related-cards">';                                                           
+            html += '<a class="c-post-title-link" href="'+allrelatedfeed[xx].link+'" title="'+allrelatedfeed[xx].title+'">';
+            html += '   <h2 class="c-event__title">';
+            html +=         allrelatedfeed[xx].title;
+            html += '   </h2>';
+            html += '</a>';
+            html += '</div>';
+            html += '</div>';
+            html += '</div>';
+            write.insertAdjacentHTML('beforeend', html)
+            if (xx >= maxrelated) {
+                break;
+            }
+            /* end if maxrelated */
+        }
+        /* if not this post */
+    }
+    /* end for */
+}
+
+if(typeof label_related !== 'undefined') {
+    for (var loop_related = 0; loop_related < label_related.length; loop_related++){
+        $.getJSON("/feeds/posts/default/-/"+ label_related[loop_related] + "?alt=json-in-script&max-results=7&callback=?",function(result){
+            extractfeed(result);
+        });
+    }
+    $(document).ajaxComplete(function () {
+        createrelated();
+        $('.related-cards-title').matchHeight();
+    });
+}
+
+/**
+ * Color Box
+ */
+ $(document).ready(function(){
+    $(".entry-content a:has(img)").colorbox({rel:'image', transition:"none", width:"75%", height:"auto"});
 }); 
