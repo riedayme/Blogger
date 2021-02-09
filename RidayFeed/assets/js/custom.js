498d3f05b5951618c9be94e611f292bccc915058
Riedayme <programmer.irfaan@gmail.com>
2021-02-09 10:30:34 +0700

    update

--- RidayFeed/assets/js/custom.js
+++ RidayFeed/assets/js/custom.js
@@ -1,25 +1,4 @@
 /**
-* Crypto.js Safelink.
-*/
-if ($.urlParam = function(n) {
-    var t = new RegExp("[?&]" + n + "=([^&#]*)").exec(window.location.href);
-    return null == t ? null : decodeURI(t[1]) || 0
-}, null != $.urlParam("from")) {
-    var countdown = $("#countdown"),
-    nextbutton = $("#nextbutton"),
-    time = safelink_time;
-    function CountDown() {
-        document.getElementById("timer").innerHTML = time, (time -= 1) < 0 ? countdown.fadeOut("slow", function() {
-            nextbutton.fadeIn()
-        }) : window.setTimeout('CountDown()', 1e3)
-    }
-    CountDown(), nextbutton.on("click", function() {
-        var n = aesCrypto.decrypt(convertstr($.urlParam("from")), convertstr("root"));
-        window.location.href = n
-    })
-}
-
-/**
 * Highlight.js
 */
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -370,6 +349,7 @@ $.extend( $.fn.dataTable.defaults, {
     'select': {
         'style': 'multi'
     },
+    'paging': false,
     'pagingType': 'simple',    
     responsive: {
         details: {
