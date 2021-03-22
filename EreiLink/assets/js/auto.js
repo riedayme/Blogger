ffaa4cf4d07a250605b9fc58a87a23df89eaf7d6
riedayme <programmer.irfaan@gmail.com>
2021-03-22 22:03:16 +0700

    update

--- EreiLink/assets/js/auto.js
+++ EreiLink/assets/js/auto.js
@@ -26,9 +26,9 @@ function convertstr(str) {
 var aesCrypto={};!function(t){"use strict";t.formatter={prefix:"",stringify:function(t){var r=this.prefix;return r+=t.salt.toString(),r+=t.ciphertext.toString()},parse:function(t){var r=CryptoJS.lib.CipherParams.create({}),e=this.prefix.length;return 0!==t.indexOf(this.prefix)?r:(r.ciphertext=CryptoJS.enc.Hex.parse(t.substring(16+e)),r.salt=CryptoJS.enc.Hex.parse(t.substring(e,16+e)),r)}},t.encrypt=function(r,e){try{return CryptoJS.AES.encrypt(r,e,{format:t.formatter}).toString()}catch(n){return""}},t.decrypt=function(r,e){try{var n=CryptoJS.AES.decrypt(r,e,{format:t.formatter});return n.toString(CryptoJS.enc.Utf8)}catch(i){return""}}}(aesCrypto);
 
 if (!setting.pengecualian) {
-	setting.pengecualian = window.location.href;
+	setting.pengecualian = window.location.hostname;
 } else {
-	setting.pengecualian += "," + window.location.href;
+	setting.pengecualian += "," + window.location.hostname;
 }
 
 var pengecualian = pengecualian();
